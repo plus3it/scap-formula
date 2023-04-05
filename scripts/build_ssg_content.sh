54829740e08859f6bb2808c910c65e1c3b83d17c
Ehinome Ihenyen <ehinome.ihenyen@gmail.com>
2023-04-05 11:10:07 -0400

    Updates to scap v1.3 and v0.1.67 of scap content
    Squashed commits:
    
    * Upgrading the cmake command string for scap v1.3
    * Removing find commands
    * Add .xml files in build/content
    * Update README.md
    * Delete build/content submodeule
    * Update cp command in build ssg content file
    * Removing .xml files
    * Updating build ssg content file to scap-v1.3
    * Updating Build SCAP 1.3 content
    * Update ComplianceAsCode project 0.1.67
    * Update scap content
    * Builds content using docker to improve reproducibility
    * Sets SOURCE_DATE_EPOCH for more reproducible builds
    * Uses ninja for faster builds
    * Captures build log as a build artifact
    * Adds ocil xml files to project to fix report generation

--- scripts/build_ssg_content.sh
+++ scripts/build_ssg_content.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 set -e
 
-# Get the parent directory of where this script is.
-SCRIPT_DIR="${BASH_SOURCE[0]}"
-while [ -h "$SCRIPT_DIR" ] ; do SCRIPT_DIR="$(readlink "$SCRIPT_DIR")"; done
-PROJECT_DIR="$( cd -P "$( dirname "$SCRIPT_DIR" )/.." && pwd )"
-
 # Set vars
 REPO=https://github.com/ComplianceAsCode/content.git
 PROFILES=( C2S stig )
 MAKE_TARGETS_RHEL=( rhel7-content rhel8-content )
 MAKE_TARGETS_OTHERS=( centos7-content centos8-content ol8-content )
-BUILD_DIR="${PROJECT_DIR}/build/content"
-DIST_DIR="${PROJECT_DIR}/scap/content/guides/openscap"
+TMPDIR="${TMPDIR:-/tmp}"
+BUILD_DIR="${TMPDIR}/ComplianceAsCode/content"
+DIST_DIR="${TMPDIR}/dist"
 
-# Remove old SSG dist directory
+export SOURCE_DATE_EPOCH=1614699939
+
+# Remove old SSG build directory
 echo "Removing directory ${BUILD_DIR}..."
-rm -rf "$BUILD_DIR"
+rm -rf "$BUILD_DIR" "$DIST_DIR"
+
+mkdir -p "$DIST_DIR"
+exec > >(tee "${DIST_DIR}/build-content.log") 2>&1
 
 # Clone the repo and checkout the latest tag
 git clone "$REPO" "$BUILD_DIR" && pushd "$BUILD_DIR"
-TAG="$(git describe --tags $(git rev-list --tags --max-count=1))"
+TAG="${SSG_VER:-$(git describe --tags $(git rev-list --tags --max-count=1))}"
 echo
 echo "Tag to build: ${TAG}"
 echo
@@ -47,20 +48,17 @@ echo "Building ssg xml content..."
 echo
 cd build
 
-# OpenSCAP content default supports SCAP 1.3. Salt installs SCAP 1.2
-# Need to build content to support SCAP 1.2
-# more info here: https://github.com/ComplianceAsCode/content/blob/master/docs/manual/developer_guide.adoc#331-building-compliant-scap-12-content
+# Build SCAP 1.3 content
+# more info here: https://complianceascode.readthedocs.io/en/latest/manual/developer/02_building_complianceascode.html#building
 
-cmake -DSSG_TARGET_OVAL_MINOR_VERSION:STRING=10 ../
+cmake -G Ninja -DSSG_TARGET_OVAL_MINOR_VERSION:STRING=11 ../
 
 # Build RHEL first to generate dependencies for CentOS and Oracle Linux
-make -j4 "${MAKE_TARGETS_RHEL[@]}"
-make -j4 "${MAKE_TARGETS_OTHERS[@]}"
+ninja -j 4 "${MAKE_TARGETS_RHEL[@]}"
+ninja -j 4 "${MAKE_TARGETS_OTHERS[@]}"
 
-find . -type f -name '*ds-1.2*' -exec rename ds-1.2.xml ds.xml '{}' \;
-find . -type f -name '*xccdf-1.2*' -exec rename xccdf-1.2.xml xccdf.xml '{}' \;
+cp *-ds.xml *-xccdf.xml *-oval.xml *-cpe-dictionary.xml *-ocil.xml "$DIST_DIR"
 
-cp *-ds.xml *-xccdf.xml *-oval.xml *-cpe-dictionary.xml "$DIST_DIR"
 echo
 echo 'Done building content!'
 
